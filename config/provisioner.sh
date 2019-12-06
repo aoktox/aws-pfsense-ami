c854a28a2c864e622d3676669464ed5568778401
Agus Prasetiyo <aoktox@users.noreply.github.com>
2019-12-06 14:54:36 +0700

    fixing missmatch interface (#3)

--- config/provisioner.sh
+++ config/provisioner.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+cp /conf/config.xml.default /conf/config.xml
+cp /conf/config.xml.default /conf.default/config.xml
+
 # replace <if></if> section of config.xml with proper names for running on AWS EC2
 sed -i -E 's$<if>em0</if>$<if>xn0</if>$g' /conf/config.xml
 sed -i -E 's$<if>em0</if>$<if>xn0</if>$g' /conf.default/config.xml
